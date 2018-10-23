b37f085d4a4f946a40506d3e9093317199d07047
Olaf Szmidt <szmidtolaf@gmail.com>
2018-10-24 00:46:42 +0100

    Change to 1-1, works fine

--- requests/models.py
+++ requests/models.py
@@ -1,22 +1,21 @@
 from django.db import models
 
 # Create your models here.
-class NFCCard(models.Model):
-    # Double check what this ID needs to be
-    # make sure they are unique
-    card_id = models.IntegerField()
-
-    def __str__(self):
-        return str(self.card_id)
-
 class Student(models.Model):
     first_name = models.CharField(max_length=30)
     second_name = models.CharField(max_length=30)
 
-    # ENSURE duplicate NFC card cannot be set here
-    # Is SET_NULL the expected behaviour for on_delete?
-    nfc_card = models.ForeignKey(NFCCard, null=True, 
-                                    on_delete=models.SET_NULL)
     def __str__(self):
         return self.first_name + " " + self.second_name
+
+class NFCCard(models.Model):
+    # Double check what this ID needs to be
+    # make sure they are unique
+    card_id = models.IntegerField()
+    student = models.OneToOneField(Student, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return str(self.card_id)
+
+
 
