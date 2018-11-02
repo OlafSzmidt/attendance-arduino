655dacbf508a11529c664bb2324a6d374e350091
Olaf Szmidt <szmidtolaf@gmail.com>
2018-11-02 21:11:25 +0000

    Change relationships in Event

--- requests/models.py
+++ requests/models.py
@@ -8,6 +8,9 @@ class Person(models.Model):
     def __str__(self):
         return self.first_name + " " + self.second_name
 
+    class Meta:
+        abstract = True
+            
 class Student(Person):
     '''Subclass of Person, no added functionality.'''
 
@@ -17,13 +20,12 @@ class Lecturer(Person):
 class Course(models.Model):
     title = models.CharField(max_length=40)
     leader = models.ForeignKey(Lecturer, null=True, on_delete=models.SET_NULL)
-    students = models.ManyToManyField(Student, null=False)
+    students = models.ManyToManyField(Student)
 
     def __str__(self):
         return self.title
 
 class NFCCard(models.Model):
-    # TODO: Double check what this ID needs to be  make sure they are unique
     card_id = models.IntegerField()
     student = models.OneToOneField(Student, null=True, on_delete=models.SET_NULL)
 
@@ -31,15 +33,11 @@ class NFCCard(models.Model):
         return str(self.card_id)
 
 class Event(models.Model):
-    name = models.ChartField(max_length=100)
     date = models.DateField(u'Date of the event', help_text=u'YYYY:MM:DD')
     start_time = models.TimeField(u'Starting time', help_text=u'HH:MM')
     end_time = models.TimeField(u'End time', help_text=u'HH:MM')
     notes = models.TextField(u'Event notes or comments', help_text=u'Event notes or comments')
+    course = models.ForeignKey(Course, blank=False, null=False, on_delete=models.PROTECT)
     
-    # HOW TO CONNECT THIS TO COURSE? MANY EVENTS TO SINGLE COURSE OBJECT. FOREIGN KEY WHICH WAY?
-    
-    # Validation ideas:
-    # end time cant be before start time. 
-    # Is course lecturer the admin of this event?
-    # Complicated: does a student have a clash at this point?
+    def __str__(self):
+        return f'{self.course.title} ({self.date} {self.start_time})'
