bccedf162dfb8edf4b2811325f15ce5af1e15328
Olaf Szmidt <szmidtolaf@gmail.com>
2018-11-03 15:22:46 +0000

    Change view and model to add attendance

--- requests/models.py
+++ requests/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.core.validators import RegexValidator
 
 class Person(models.Model):
+    '''Abstract model to be inherited by Student or Lecturer'''
     first_name = models.CharField(max_length=30,
                                   validators=[
                                     RegexValidator(
@@ -35,6 +36,8 @@ class Lecturer(Person):
     '''Subclass of Person, no added functionality.'''
 
 class Course(models.Model):
+    '''Course which is being taught by a single lecturer and attended by many
+    students. Not to be confused with Event.'''
     title = models.CharField(max_length=40)
     leader = models.ForeignKey(Lecturer, null=True, on_delete=models.SET_NULL)
     students = models.ManyToManyField(Student)
@@ -43,6 +46,8 @@ class Course(models.Model):
         return self.title
 
 class NFCCard(models.Model):
+    '''An NFC card which is being scanned by the arduino and is connected to a
+    single student'''
     card_id = models.IntegerField()
     student = models.OneToOneField(Student, null=True, on_delete=models.SET_NULL)
 
@@ -50,6 +55,7 @@ class NFCCard(models.Model):
         return f'{self.card_id} ({self.student.first_name} {self.student.second_name})'
 
 class Event(models.Model):
+    '''A single event occurance of a Course at a particular date and time'''
     date = models.DateField(u'Date of the event', help_text=u'YYYY:MM:DD')
     start_time = models.TimeField(u'Starting time', help_text=u'HH:MM')
     end_time = models.TimeField(u'End time', help_text=u'HH:MM')
@@ -58,3 +64,11 @@ class Event(models.Model):
 
     def __str__(self):
         return f'{self.course.title} ({self.date} {self.start_time})'
+
+class Attendance(models.Model):
+    '''Attendance model is used to output results once students have marked
+    themselves'''
+    student = models.ForeignKey(Student)
+    event = models.ForeignKey(Event)
+    attended = models.BooleanField(default=False)
+    
