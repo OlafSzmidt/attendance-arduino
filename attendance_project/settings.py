20bda1a2db4e1f8f80ac383f537416c9d9adc6d7
Olaf Szmidt <szmidtolaf@gmail.com>
2018-12-05 19:29:30 +0000

    Add logout functionality

--- attendance_project/settings.py
+++ attendance_project/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = []
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Application definition
 
