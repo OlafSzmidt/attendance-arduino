159b2bd5e3dee0163f9b0b326c9b5f124bba0083
Olaf Szmidt <szmidtolaf@gmail.com>
2019-02-17 19:40:01 +0000

    Arduino code!

--- arduino/ReadTag/ReadTag.ino
+++ arduino/ReadTag/ReadTag.ino
@@ -1,4 +1,3 @@
-
 #include <SPI.h>
 #include <PN532_SPI.h>
 #include <PN532.h>
@@ -6,11 +5,17 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 #include "WiFly.h"
+#include "HTTPClient.h"
+
 
 // Network Settings!
 #define SSID      "OLKAROLKA123"
 #define KEY       "85386C52C2"
+// WIFLY_AUTH_OPEN / WIFLY_AUTH_WPA1 / WIFLY_AUTH_WPA1_2 / WIFLY_AUTH_WPA2_PSK
 #define AUTH      WIFLY_AUTH_WPA2_PSK
+#define HTTP_GET_URL "http://httpbin.org/get?hello=world"
+#define HTTP_POST_URL "http://192.168.1.248:8000/cardScan/"
+
 
 // NFC init
 PN532_SPI pn532spi(SPI, 10);
@@ -19,6 +24,10 @@ NfcAdapter nfc = NfcAdapter(pn532spi);
 // Wifi init
 SoftwareSerial uart(2, 3);
 WiFly wifly(&uart);
+HTTPClient http;
+char get;
+
+char json[400];
 
 void setup(void) {
     uart.begin(9600);
@@ -26,6 +35,7 @@ void setup(void) {
     Serial.begin(9600);
     
     Serial.println("Attendance Monitor");
+    
     // wifly needs a delay for initlization.
     delay(3000);
 
@@ -41,7 +51,16 @@ void setup(void) {
       Serial.println("Network connection failed");
     }
     
+    delay(5000);
+
     nfc.begin();
+
+    // Command disables welcome message from Seeed Studio WiFi shield.
+    // The welcome message can interrupt the POST request later and wrong bytes
+    // are sent over wifi. The server is unable to read the incoming request
+    // and the whole thing errors. In this case WiFi Shield sends a "*HELLO*" stream
+    // which would often be appended to the method type in the HTTP library.
+    wifly.sendCommand("set comm remote 0\r");
 }
 
 void loop(void) {
@@ -50,6 +69,35 @@ void loop(void) {
     {
         NfcTag tag = nfc.read();
         Serial.println(tag.getUidString());
+
+        // There are C++ libraries that can construct JSON objects or strings
+        // but we are trying to keep the arduino as lightweight as possible so
+        // constructing it ourselves. However, no standard library (std) in the Uno
+        // means this is difficult. 
+
+        // Note: sprintf is unsafe
+        
+        Serial.println("\r\n\r\nTry to post data to url - " HTTP_POST_URL);
+        Serial.println("-------------------------------");
+
+
+        String stringTag = tag.getUidString();
+        
+        // sizeof was returning 6 for the tag... Different std library causing confusion
+        // built in length() used instead: https://www.arduino.cc/en/Tutorial/StringLength     
+        // Is +1 needed?
+        char __tag[stringTag.length() + 1];
+
+        Serial.println(sizeof(__tag));
+        
+        stringTag.toCharArray(__tag, sizeof(__tag));
+        
+        while (http.post(HTTP_POST_URL, __tag, 10000) < 0) {
+        }
+        while (wifly.receive((uint8_t *)&get, 1, 1000) == 1) {
+          Serial.print(get);
+        }
     }
+    
     delay(1500);
 }
